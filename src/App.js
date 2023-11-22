fbdc0ab90bb38564c5f6167bf6bd07dce9ec2c4b
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-11-22 07:45:20 -0800

    I create main board of game with 9 squares!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import "./App.css";
-
-function App() {
-  return <div className="bg-red-300">hello tictactoe1</div>;
+export default function Board() {
+  return (
+    <div className="flex flex-col p-32 justify-center items-center">
+      <div>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          1
+        </button>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          2
+        </button>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          3
+        </button>
+      </div>
+      <div>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          4
+        </button>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          5
+        </button>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          6
+        </button>
+      </div>
+      <div>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          7
+        </button>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          8
+        </button>
+        <button className="px-2 w-20 h-20 text-xl font-bold border-[0.1rem] border-gray-900">
+          9
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default App;
